d5c3b9330e1e9c466eb3f795eee02c2ed6ee593c
nishanthapa <nishanthaps112@gmail.com>
2025-02-10 21:41:40 +0545

    navbar

--- food/src/Components/Navbar/Navbar.jsx
+++ food/src/Components/Navbar/Navbar.jsx
@@ -53,8 +53,31 @@ const Dropdownlist = [
 
 const Navbar = ({handleOrderPopup}) => {
   return (
-    <div></div>
-  )
-}
+    <div className="shadow-md bg-white dark: bg-text-white
+    duration-200 relative z-40" >
+        <div className="bg-primary/40 py-2">
+        <div className="container flex justify-between
+         items-center">
+            <div>
+                <a href="/#" className="text-xl font-bold items-center flex gap-1">
+                    <FiShoppingBag size={30} />
+                    shopme
+                </a>
+            </div>
+            <div className="flex justify-between items-center gap-4">
+                <div className="relative group hidden sm:block ">
+                    <input type="text" placeholder="search" className="w-[200px]
+                     sm:[200px] group-hover:w-[300px] transition-all duration-300
+                     rounded-lg border border-gray-300 py-1 px-1 text-sm
+                     focus:outline-none focus-border-1 
+                     focus:border-primary dark:border-gray-500 dark:bg-slate-800"/>
+                    </div>
+                </div>
+            </div>
+         </div>
 
-export default Navbar
+    </div>
+  );
+};
+
+export default Navbar;
