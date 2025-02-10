e96eeec4d34f0373fdad0cc1b2808455b06a0431
nishanthapa <nishanthaps112@gmail.com>
2025-02-10 21:05:35 +0545

    nav

---
+++ food/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import {IoMdSearch} from "react-icons/io";
+import {FaCartShopping} from "react-icons/fa6";
+import {FaCaretDown} from "react-icons/fa";
+import{FiShoppingBag} from "react-icons/fi";
+
+
+const Menu = [
+    {
+        id: 1,
+        name: "Home",
+        link: "/#",
+
+    },
+    {
+        id: 2,
+        name: "Top Rated",
+        link: "/#services",
+    },
+    {
+        id: 3,
+        name: "Kids Wear",
+        link: "/#",
+    },
+    {
+        id: 3,
+        name: "Mens Wear",
+        link: "/#",
+    },
+    {
+        
+        id: 3,
+        name: "Electronics",
+        link: "/#",
+    },
+    
+];
+
+const Dropdownlist = [
+    {
+
+        id: 1,
+        name: "Trending Products ",
+        link: "/#",
+    },
+    {
+        id: 2,
+        name: "Best Selling",
+        link: "/#",
+    },
+];
+
+
+const Navbar = ({handleOrderPopup}) => {
+  return (
+    <div></div>
+  )
+}
+
+export default Navbar
